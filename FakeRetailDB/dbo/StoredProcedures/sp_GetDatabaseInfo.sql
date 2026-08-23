a311da4829a66d9d77cd6c5f8cf213799ed13094
Winston53 <22044947A@common.cpce-polyu.edu.hk>
2026-08-23 21:28:24 +0800

    <SP>: For CI testing

---
+++ FakeRetailDB/dbo/StoredProcedures/sp_GetDatabaseInfo.sql
@@ -0,0 +1,12 @@
+-- Testing
+CREATE PROCEDURE [dbo].[sp_GetDatabaseInfo]
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT 
+        DB_NAME() AS DatabaseName,
+        @@SERVERNAME AS ServerName,
+        GETDATE() AS CurrentDateTime,
+        'PR validation test' AS Note;
+END;
